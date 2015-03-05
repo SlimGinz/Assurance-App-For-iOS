5b7cd329d2b5abc9f67aeb31f4478bf18bad9f42
AppMaker123 <cory.ginsberg1@gmail.com>
2015-03-05 16:38:32 -0600

    Initial commit from local git

---
+++ Assurance/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Assurance
+//
+//  Created by Cory Ginsberg on 3/5/15.
+//  Copyright (c) 2015 Boiling Point Development. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Assurance/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Assurance
+//
+//  Created by Cory Ginsberg on 3/5/15.
+//  Copyright (c) 2015 Boiling Point Development. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
